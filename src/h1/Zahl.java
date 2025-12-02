457d3426141db08a59f4869a849d7822c7a36286
Max <maxsiepsiak238@gmail.com>
2025-12-02 16:37:31 +0100

    Init h1

---
+++ src/h1/Zahl.java
@@ -0,0 +1,30 @@
+package h1;
+
+public class Zahl {
+    public boolean even = false, small = true, positive = true;
+    public int num = 12;
+
+    public void setEven() {
+        if(num % 2 == 0) {
+            even = true;
+        } else {
+            even = false;
+        }
+    }
+
+    public void setSmall() {
+        if(num < 100) {
+            small = true;
+        } else {
+            small = false;
+        }
+    }
+
+    public void setPositive() {
+        if(num > 0) {
+            positive = true;
+        } else {
+            positive = false;
+        }
+    }
+}
